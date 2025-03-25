af8e7cf23a2ef9535d15ac237fd7bddb8852516b
Medha Bhardwaj <143182673+medha-14@users.noreply.github.com>
2025-03-25 21:12:49 +0530

    Create model1.py

---
+++ model1.py
@@ -0,0 +1,10 @@
+import pybamm
+
+model = pybamm.BaseModel()
+x = pybamm.Variable("x")
+y = pybamm.Variable("y")
+dxdt = 4 * x - 2 * y
+dydt = 3 * x - y
+model.rhs = {x: dxdt, y: dydt}
+model.initial_conditions = {x: pybamm.Scalar(1), y: pybamm.Scalar(2)}
+model.variables = {"x": x, "y": y, "z": x + 4 * y}
